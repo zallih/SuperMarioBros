fe6a60a2f1e58e3c0dc7ead533e2630dfe321737
lailaz <88406056+zallih@users.noreply.github.com>
2021-10-27 15:18:15 -0300

    Add files via upload

--- SuperMario/src/com/ltztec/entities/Player.java
+++ SuperMario/src/com/ltztec/entities/Player.java
@@ -1,6 +1,5 @@
 package com.ltztec.entities;
 
-
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -10,34 +9,37 @@ import com.ltztec.main.Game;
 import com.ltztec.world.Camera;
 import com.ltztec.world.World;
 
+public class Player extends Entity {
 
-public class Player extends Entity{
-	
 	public boolean right, left;
-	public double speed = 1.1;
+	public double speed = 1.2;
 	private double gravity = 1.2;
-	
+
 	public boolean jump = false;
 	public boolean isJumping = false;
 	public int jumpHeight = 32;
 	public int jumpFrames = 0;
-	
+
 	public int right_dir = 0, left_dir = 1;
 	public int dir = right_dir;
-	
+
 	private int frames = 0, maxFrames = 8, index = 0, maxIndex = 2;
 	private boolean moved = false;
 	private BufferedImage[] rightPlayer;
 	private BufferedImage[] leftPlayer;
-	
-	
-	public Player(int x, int y, int width, int height,double speed,BufferedImage sprite) {
-		super(x, y, width, height,speed,sprite);
-		
+
+	private BufferedImage jumpRightPlayer;
+	private BufferedImage jumpLeftPlayer;
+
+	public Player(int x, int y, int width, int height, double speed, BufferedImage sprite) {
+		super(x, y, width, height, speed, sprite);
+
 		rightPlayer = new BufferedImage[3];
 		leftPlayer = new BufferedImage[3];
-		
-		
+
+		jumpRightPlayer = Game.spritesheet.getSprite(48, 0, 16, 16);
+		jumpLeftPlayer = Game.spritesheet.getSprite(48, 16, 16, 16);
+
 		for (int i = 0; i < 3; i++) {
 			rightPlayer[i] = Game.spritesheet.getSprite(80 + (i * 16), 0, 16, 16);
 		}
@@ -45,13 +47,13 @@ public class Player extends Entity{
 			leftPlayer[i] = Game.spritesheet.getSprite(80 + (i * 16), 16, 16, 16);
 		}
 	}
-	
-	public void tick(){
-		
-		if(World.isFree((int)x, (int)(y+gravity)) && isJumping == false) {
-			y+=gravity;
+
+	public void tick() {
+
+		if (World.isFree((int) x, (int) (y + gravity)) && isJumping == false) {
+			y += gravity;
 		}
-		
+
 		depth = 2;
 		moved = false;
 		if (right && World.isFree((int) (x + speed), this.getY())) {
@@ -63,7 +65,7 @@ public class Player extends Entity{
 			dir = left_dir;
 			x -= speed;
 		}
-		
+
 		if (moved) {
 			frames++;
 			if (frames == maxFrames) {
@@ -74,23 +76,20 @@ public class Player extends Entity{
 				}
 			}
 		}
-		
-		
-		
-		
-		if(jump == true) {
-			if(!World.isFree(this.getX(), this.getY()+1)) {
+
+		if (jump == true) {
+			if (!World.isFree(this.getX(), this.getY() + 1)) {
 				isJumping = true;
-			}else {
+			} else {
 				jump = false;
 			}
 		}
-		
-		if(isJumping == true) {
-			if(World.isFree(this.getX(), this.getY()-2)) {
-				y-=2;
-				jumpFrames+=2;
-				if(jumpFrames == jumpHeight) {
+
+		if (isJumping == true) {
+			if (World.isFree(this.getX(), this.getY() - 2)) {
+				y -= 2;
+				jumpFrames += 2;
+				if (jumpFrames == jumpHeight) {
 					isJumping = false;
 					jump = false;
 					jumpFrames = 0;
@@ -101,25 +100,22 @@ public class Player extends Entity{
 				jumpFrames = 0;
 			}
 		}
-		
-		
-		
-	}
-	
 
-	
+	}
 
 	public void render(Graphics g) {
-	
-			if (dir == right_dir) {
-				g.drawImage(rightPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
-				
-			} else if (dir == left_dir) {
-				g.drawImage(leftPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
-				
-			}
-	}
-	
 
+		if (dir == right_dir && isJumping == false) {
+			g.drawImage(rightPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+		} else if (dir == left_dir && isJumping == false) {
+			g.drawImage(leftPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+		}
+
+		if (isJumping == true && dir == left_dir) {
+			g.drawImage(jumpLeftPlayer, this.getX() - Camera.x, this.getY() - Camera.y, null);
+		} else if (isJumping == true && dir == right_dir) {
+			g.drawImage(jumpRightPlayer, this.getX() - Camera.x, this.getY() - Camera.y, null);
+		}
+	}
 
 }
